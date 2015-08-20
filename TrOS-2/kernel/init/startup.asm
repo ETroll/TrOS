54d335548fa258ef833b2ae1388430639bb71e8a
ETroll <karl.syvert@myerart.net>
2015-08-20 10:33:01 +0200

    minor cleanup in startup file

--- TrOS-2/kernel/init/startup.asm
+++ TrOS-2/kernel/init/startup.asm
@@ -15,19 +15,19 @@ extern kernel_main
 
 global kernel_init
 kernel_init:
-	mov	dx, 0x10
-	mov	ds, dx
-	mov	ss, dx
-	mov	es, dx
-	mov	esp, stack_top;0x90000 ; reset stack back to where we started and overwrite
+	mov dx, 0x10
+	mov ds, dx
+	mov ss, dx
+	mov es, dx
+	mov esp, stack_top;0x90000 ; reset stack back to where we started and overwrite
 
 	; Set up the itsy bisty tinyest "stackframe" before calling kernel
 	mov ebp,esp
     push ebp
 
 	push stack_top+8
-	push eax		; multiboot "magic"
-	push ebx		; multiboot struct pointer
+	push eax ; multiboot "magic"
+	push ebx ; multiboot struct pointer
 
 	call kernel_main
 
